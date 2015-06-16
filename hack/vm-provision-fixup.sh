59d2d740d8e062d221d0b8aa2349d3e3133b7490
Jhon Honce <jhonce@redhat.com>
2015-06-15 17:18:54 -0700

    Fixup KUBECONFIG in /etc/profile.d/openshift.sh

--- hack/vm-provision-fixup.sh
+++ hack/vm-provision-fixup.sh
@@ -8,3 +8,15 @@ if [[ -f "${SCRIPT}" ]]; then
     sed -i -e '/^options.*$/d' /etc/resolv.conf
 fi
 unset SCRIPT
+
+# Add new configuration Environment variable
+SCRIPT=/etc/profile.d/openshift.sh
+if ! grep -q KUBECONFIG $SCRIPT; then
+    echo "Adding KUBECONFIG to $SCRIPT"
+    if grep -q OPENSHIFTCONFIG $SCRIPT; then
+        echo 'export KUBECONFIG=$OPENSHIFTCONFIG' >>$SCRIPT
+    else
+        echo 'export KUBECONFIG=/openshift.local.config/master/admin.kubeconfig' >>$SCRIPT
+    fi
+fi
+unset SCRIPT
